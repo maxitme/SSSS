f79c94911aa4b4c443d6a0d4b7d76be8b564aece
thinhthinh <thinhnh198@gmail.com>
2024-04-22 15:15:42 +0700

    sss

--- src/app/(admin)/customer/Search.tsx
+++ src/app/(admin)/customer/Search.tsx
@@ -1,38 +1,35 @@
-'use client';
-
-import { CiSearch } from 'react-icons/ci';
-import { useSearchParams , usePathname , useRouter } from 'next/navigation';
-
-
-export default function Search({ placeholder }: { placeholder: string }) {
+"use client";
+ 
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce"; 
+ 
+const Search = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const {replace} = useRouter();
-  function handleSearch(term: string) {
+  const { replace } = useRouter();
+ 
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(term);
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('query', term);
+        params.set("query", term);
     } else {
-      params.delete('query');
+      params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-    console.log(term);
-  }
-
+  }, 300);
+ 
   return (
-    <div className="relative flex flex-1 ml-9 mr-9">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
+    <div className="relative flex flex-1 ml-9 mr-9 border-1">
       <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={e => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('query')?.toString()}
+        type="text"
+        className="input input-bordered input-accent w-full"
+        placeholder="Search..."
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
       />
-      <CiSearch className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
-}
+};
+ 
+export default Search;
